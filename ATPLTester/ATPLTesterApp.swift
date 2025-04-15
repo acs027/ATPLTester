3ee97cd914914cce087fe9dbe8c8edb311db1491
acs027 <alicihansarac@gmail.com>
2025-04-15 16:51:01 +0300

    Initial Commit

---
+++ ATPLTester/ATPLTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATPLTesterApp.swift
+//  ATPLTester
+//
+//  Created by ali cihan on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ATPLTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
